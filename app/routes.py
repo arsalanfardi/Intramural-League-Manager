6eee123a12cda65aec72c032ff97bcc40d508d7b
karimbounekhla <karim.bounekhla@mail.utoronto.ca>
2020-03-18 12:43:20 -0600

    Added SQL connection and requirements.txt

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from cs50 import SQL
 from app import app
 
 # Ensure templates are auto-reloaded
@@ -14,7 +14,14 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+# Configure SQLite database
+db = SQL("sqlite:///app/league.db")
+
 @app.route('/')
 @app.route('/index')
 def index():
+    test = db.execute(
+        "SELECT * FROM league"
+    )
+    print(test)
     return render_template("index.html", var1=None, var2=None, var3=None)
